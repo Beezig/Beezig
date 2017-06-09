bca2458d0418a6807a25bb2b62d4df07a230f657
RoccoDev <admin@roccodeveloping.tk>
2017-06-09 13:23:30 +0200

    Added "x days ago" to /seen

--- src/tk/roccodev/zta/command/SeenCommand.java
+++ src/tk/roccodev/zta/command/SeenCommand.java
@@ -1,11 +1,12 @@
 package tk.roccodev.zta.command;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 import eu.the5zig.mod.The5zigAPI;
 import tk.roccodev.zta.IHive;
 import tk.roccodev.zta.Log;
-import tk.roccodev.zta.ZTAMain;
 import tk.roccodev.zta.hiveapi.HiveAPI;
-import tk.roccodev.zta.notes.NotesManager;
 
 public class SeenCommand implements Command{
 
@@ -30,7 +31,12 @@ public class SeenCommand implements Command{
 			new Thread(new Runnable(){
 				@Override
 				public void run(){
-					The5zigAPI.getAPI().messagePlayer(Log.info + HiveAPI.getRankColor(HiveAPI.getNetworkRank(ign)) + ign + "§e was last seen on§a " + HiveAPI.getLastLogout(ign) + "§e.");
+					Date now = new Date();
+					Date lastSeen = HiveAPI.getLastLogout(ign);
+					long diff = now.getTime() - lastSeen.getTime();
+					
+					
+					The5zigAPI.getAPI().messagePlayer(Log.info + HiveAPI.getRankColor(HiveAPI.getNetworkRank(ign)) + ign + "§e was last seen on§a " + lastSeen + "§e (§a" + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS) + " days ago§e).");
 				}
 			}).start();
 			
