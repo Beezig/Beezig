6c8aa7bde22efc1a46a15e55620cb52813d03f30
RoccoDev <admin@roccodeveloping.tk>
2017-06-12 16:28:49 +0200

    First attempt at autovoting.
    - Note: I haven't registered the command yet.

---
+++ src/tk/roccodev/zta/autovote/AutovoteUtils.java
@@ -0,0 +1,81 @@
+package tk.roccodev.zta.autovote;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.yaml.snakeyaml.Yaml;
+
+import tk.roccodev.zta.ZTAMain;
+
+public class AutovoteUtils {
+
+	
+	private static Yaml yml = new Yaml();
+	private static HashMap<String, Object> ymlObject;
+	
+	
+	public static List<String> getMapsForMode(String mode){
+		List<String> tr = new ArrayList<String>();
+		return tr;
+	}
+	
+	
+	public static void load(){
+		InputStream input = null;
+		try {
+			input = new FileInputStream(new File(ZTAMain.mcFile + "/autovote.yml"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ymlObject = (HashMap<String, Object>) yml.load(input);
+		
+		
+		try {
+			input.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void set(String key, Object value){
+		ymlObject.put(key, value);
+	}
+	
+	public static Object get(String key){
+		return ymlObject.get(key);
+	}
+	
+	
+	public static void dump(){
+		FileWriter wr = null;
+		try {
+			wr = new FileWriter(new File(ZTAMain.mcFile + "/autovote.yml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		yml.dump(ymlObject, wr);
+		
+		
+		
+		
+		try {
+			wr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ src/tk/roccodev/zta/command/AutoVoteCommand.java
@@ -0,0 +1,90 @@
+package tk.roccodev.zta.command;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.the5zig.mod.The5zigAPI;
+import tk.roccodev.zta.IHive;
+import tk.roccodev.zta.Log;
+import tk.roccodev.zta.ZTAMain;
+import tk.roccodev.zta.autovote.AutovoteUtils;
+import tk.roccodev.zta.hiveapi.DRMap;
+import tk.roccodev.zta.hiveapi.TIMVMap;
+
+public class AutoVoteCommand implements Command{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return "autovote";
+	}
+
+	@Override
+	public String[] getAliases() {
+		String[] aliases = {"/autovote", "/avote"};
+		return aliases;
+	}
+	
+
+	@Override
+	public boolean execute(String[] args) {
+		
+		if(!ZTAMain.isTIMV && !ZTAMain.isDR) return false;
+		if(!(The5zigAPI.getAPI().getActiveServer() instanceof IHive)) return false;
+		if(args.length == 0){
+			//TODO List maps
+		}
+		//Format would be /autovote add dr_throwback
+		if(args.length == 2){
+			String mode = args[0];
+			if(mode.equalsIgnoreCase("add")){
+				String map = args[1];
+				String[] data = map.split("_");
+				String gamemode = data[0]; // ex: dr
+				String mapString = data[1]; //ex: throwback
+				if(gamemode.equalsIgnoreCase("dr")){
+					DRMap apiMap = null;
+					try{
+						apiMap = DRMap.valueOf(mapString);
+					}
+					catch(IllegalArgumentException e){
+						The5zigAPI.getAPI().messagePlayer(Log.error + "Map not found.");
+						return true;
+					}
+					
+					
+					List<String> drMaps = (List<String>) AutovoteUtils.get("dr");
+					if(drMaps == null) drMaps = new ArrayList<String>();
+					drMaps.add(apiMap.name());
+					AutovoteUtils.set("dr", drMaps);
+					AutovoteUtils.dump();
+				}
+				else if(gamemode.equalsIgnoreCase("timv")){
+					TIMVMap apiMap = null;
+					try{
+						apiMap = TIMVMap.valueOf(mapString);
+					}
+					catch(IllegalArgumentException e){
+						The5zigAPI.getAPI().messagePlayer(Log.error + "Map not found.");
+						return true;
+					}
+					
+					
+					List<String> timvMaps = (List<String>) AutovoteUtils.get("timv");
+					if(timvMaps == null) timvMaps = new ArrayList<String>();
+					timvMaps.add(apiMap.name());
+					AutovoteUtils.set("timv", timvMaps);
+					AutovoteUtils.dump();
+				}
+			}
+			
+		}
+		
+		return true;
+	}
+
+	
+
+	
+
+}
