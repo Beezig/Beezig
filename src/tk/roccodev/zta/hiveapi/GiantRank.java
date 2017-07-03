ab23e411107b523a5673aeb51143382a364b3af0
RoccoDev <admin@roccodeveloping.tk>
2017-07-03 17:12:52 +0200

    Added GNT ranks enum

---
+++ src/tk/roccodev/zta/hiveapi/GiantRank.java
@@ -0,0 +1,47 @@
+package tk.roccodev.zta.hiveapi;
+import static eu.the5zig.util.minecraft.ChatColor.AQUA;
+import static eu.the5zig.util.minecraft.ChatColor.GOLD;
+import static eu.the5zig.util.minecraft.ChatColor.GRAY;
+import static eu.the5zig.util.minecraft.ChatColor.GREEN;
+import static eu.the5zig.util.minecraft.ChatColor.RED;
+import static eu.the5zig.util.minecraft.ChatColor.YELLOW;
+
+public enum GiantRank {
+	
+	DWARF("Dwarf", GRAY + ""),
+	LITTLEJOHN("Little John", GOLD + ""),
+	GENTLEGIANT("Gentle Giant", "§d"),
+	COLOSSAL("Colossal", AQUA + ""),
+	GALACTUS("Galactus", YELLOW + ""),
+	BEHEMOTH("Behemoth", GREEN + ""),
+	GRAWP("Grawp", RED + ""),
+	ANDRE("Andre", "§9"),
+	CYCLOPS("Cyclops", "§5"),
+	BIGFRIENDLYGIANT("Big Friendly Giant", "§6§l"),
+	GULLIVER("Gulliver", "§b§l"),
+	BIGFOOT("Bigfoot", "§e§l"),
+	TITAN("Titan", "§a§l"),
+	HAGRID("Hagrid", "§c§l"),
+	GOLIATH("Goliath", "§c§l"),
+	SKYGIANT("✹ Sky Giant", "§d§l");
+	
+	private String display, prefix;
+	
+	GiantRank(String display, String prefix){
+		this.display = display;
+		this.prefix = prefix;
+	}
+
+	public String getDisplay() {
+		return display;
+	}
+
+	public String getPrefix() {
+		return prefix;
+	}
+	
+	public String getTotalDisplay(){
+		return prefix + display;
+	}
+
+}
