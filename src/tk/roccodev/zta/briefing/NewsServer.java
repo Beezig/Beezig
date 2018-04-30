a49870ee1b7c0930daeb7a02b91c1cb57c0d3e67
RoccoDev <roccodev.business@gmail.com>
2018-04-30 15:37:34 +0200

    Made the briefing toggleable

--- src/tk/roccodev/zta/briefing/NewsServer.java
+++ src/tk/roccodev/zta/briefing/NewsServer.java
@@ -7,25 +7,22 @@ import java.util.Map;
 
 import eu.the5zig.mod.The5zigAPI;
 import eu.the5zig.util.minecraft.ChatColor;
-import tk.roccodev.zta.Log;
 import tk.roccodev.zta.briefing.lergin.NewMap;
 import tk.roccodev.zta.briefing.lergin.StaffChangeType;
 import tk.roccodev.zta.briefing.lergin.StaffUpdate;
+import tk.roccodev.zta.settings.Setting;
 
 public class NewsServer {
 
 	public static void serveNews(ArrayList<News> news, ArrayList<NewMap> maps, ArrayList<StaffUpdate> staff) {
-		try {
-			Thread.sleep(200);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		if (!Setting.BRIEFING.getValue())
+			return;
+		if (news.size() == 0 && maps.size() == 0 && staff.size() == 0)
+			return;
 
-		if(news.size() == 0 && maps.size() == 0 && staff.size() == 0) return;
-		
-		The5zigAPI.getAPI().messagePlayer("§f                     §b§m                  §f §f§lBeezig Briefing§f §b§m                  "
-				+ (news.size() == 0 ? "" : "- " + ChatColor.ITALIC + "\n\n§fOur news:"));
+		The5zigAPI.getAPI().messagePlayer(
+				"§f                     §b§m                  §f §f§lBeezig Briefing§f §b§m                  "
+						+ (news.size() == 0 ? "" : "- " + ChatColor.ITALIC + "\n\n§fOur news:"));
 		if (news.size() != 0) {
 			for (News n : news) {
 				The5zigAPI.getAPI().messagePlayer("\n§e" + ChatColor.UNDERLINE + n.getTitle());
@@ -38,23 +35,20 @@ public class NewsServer {
 			The5zigAPI.getAPI().messagePlayer("\n - " + ChatColor.ITALIC + "New maps:");
 			StringBuilder sb = new StringBuilder();
 			HashMap<String, ArrayList<NewMap>> grouped = new HashMap<String, ArrayList<NewMap>>();
-			for(NewMap m : maps) {
-				
-				if(!grouped.containsKey(m.getGameMode())) {
+			for (NewMap m : maps) {
+
+				if (!grouped.containsKey(m.getGameMode())) {
 					grouped.put(m.getGameMode(), new ArrayList<NewMap>(Arrays.asList(m)));
-				}
-				else {
+				} else {
 					ArrayList<NewMap> tmp = grouped.get(m.getGameMode());
-					 tmp.add(m);
-					 grouped.put(m.getGameMode(), tmp);
+					tmp.add(m);
+					grouped.put(m.getGameMode(), tmp);
 				}
-				
-				
-				
+
 			}
-			for(Map.Entry<String, ArrayList<NewMap>> e : grouped.entrySet()) {
+			for (Map.Entry<String, ArrayList<NewMap>> e : grouped.entrySet()) {
 				sb.append("§a" + e.getKey() + ":§e ");
-				for(NewMap m : e.getValue()) {
+				for (NewMap m : e.getValue()) {
 					sb.append(m.getName() + ", ");
 				}
 				sb.trimToSize();
@@ -63,37 +57,32 @@ public class NewsServer {
 				sb.append(".");
 				sb.append("\n");
 			}
-		
+
 			The5zigAPI.getAPI().messagePlayer("§e" + sb.toString().trim());
-			
-		
-			
+
 		}
-		if(staff.size() != 0) {
+		if (staff.size() != 0) {
 			The5zigAPI.getAPI().messagePlayer("\n - " + ChatColor.ITALIC + "Staff changes:");
 			StringBuilder sb = new StringBuilder();
 			HashMap<StaffChangeType, ArrayList<StaffUpdate>> grouped = new HashMap<StaffChangeType, ArrayList<StaffUpdate>>();
-			for(StaffUpdate s : staff) {
-				
-				if(!grouped.containsKey(s.getType())) {
-					
+			for (StaffUpdate s : staff) {
+
+				if (!grouped.containsKey(s.getType())) {
+
 					grouped.put(s.getType(), new ArrayList<StaffUpdate>(Arrays.asList(s)));
-					
+
+				} else {
+					ArrayList<StaffUpdate> tmp = grouped.get(s.getType());
+					tmp.add(s);
+					grouped.put(s.getType(), tmp);
+
 				}
-				else {
-					 ArrayList<StaffUpdate> tmp = grouped.get(s.getType());
-					 tmp.add(s);
-					 grouped.put(s.getType(), tmp);
-					
-				}
-				
-				
-				
+
 			}
-			
-			for(Map.Entry<StaffChangeType, ArrayList<StaffUpdate>> e : grouped.entrySet()) {
+
+			for (Map.Entry<StaffChangeType, ArrayList<StaffUpdate>> e : grouped.entrySet()) {
 				sb.append(e.getKey().getDisplay() + ":§e ");
-				for(StaffUpdate s : e.getValue()) {
+				for (StaffUpdate s : e.getValue()) {
 					sb.append(s.getStaffName() + ", ");
 				}
 				sb.trimToSize();
@@ -102,12 +91,14 @@ public class NewsServer {
 				sb.append(".");
 				sb.append("\n");
 			}
-		
+
 			The5zigAPI.getAPI().messagePlayer("§e" + sb.toString().trim());
-			
+
 		}
-		The5zigAPI.getAPI().messagePlayer("\n§f     §b§m            §f §7Map & staff data gently provided by Lergin§f §b§m            ");
-		The5zigAPI.getAPI().messagePlayer("§f       §b§m                                  §f §7hive.lergin.de§f §b§m                                   ");
+		The5zigAPI.getAPI().messagePlayer(
+				"\n§f     §b§m            §f §7Map & staff data gently provided by Lergin§f §b§m            ");
+		The5zigAPI.getAPI().messagePlayer(
+				"§f       §b§m                                  §f §7hive.lergin.de§f §b§m                                   ");
 	}
 
 }
