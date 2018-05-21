f7f63f6a795db4f09bd23432fd93404cdd9cffa2
Malte <hey@lergin.de>
2018-05-21 12:21:28 +0200

    changed the api endpoint for team and map updates

--- src/tk/roccodev/zta/briefing/fetcher/Links.java
+++ src/tk/roccodev/zta/briefing/fetcher/Links.java
@@ -3,6 +3,6 @@ package tk.roccodev.zta.briefing.fetcher;
 public class Links {
 
 	public static String OUR_NEWS = "https://roccodev.pw/beezighosting/news/news.json";
-	public static String LERGIN_STAFF = "https://lergin.de/api/hive/team/0";
-	public static String LERGIN_MAP = "https://lergin.de/api/hive/maps";
+	public static String LERGIN_STAFF = "https://api.lergin.de/hive/team/0";
+	public static String LERGIN_MAP = "https://api.lergin.de/hive/maps";
 }
