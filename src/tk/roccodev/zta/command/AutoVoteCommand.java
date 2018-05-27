dd98d6718bc4424c469ab71b6b11787d95d21f77
RoccoDev <roccodev.business@gmail.com>
2018-05-27 16:20:00 +0200

    /autovote place (Needs implementation inside the listeners)

--- src/tk/roccodev/zta/command/AutoVoteCommand.java
+++ src/tk/roccodev/zta/command/AutoVoteCommand.java
@@ -81,11 +81,43 @@ public class AutoVoteCommand implements Command {
 
 			}
 
+		} else if (args.length == 3 && args[0].equalsIgnoreCase("place")) {
+			//e.g, /autovote place timv_azure_island 1
+			String map = args[1];
+			String[] data = map.split("_");
+			String gamemode = data[0]; // ex: dr
+			List<String> mapStr = new ArrayList<>(Arrays.asList(data));
+			mapStr.remove(0);
+			StringBuilder sb = new StringBuilder();
+			String mapString;
+			for (String s : mapStr) {
+				sb.append(s + "_");
+			}
+			mapString = sb.deleteCharAt(sb.length() - 1).toString().trim();
+			ArrayList<String> maps = new ArrayList<>(AutovoteUtils.getMapsForMode(gamemode.toLowerCase()));
+			int index = Integer.parseInt(args[2]);
+			String tmp = maps.get(index - 1);
+			int tmpIndex = maps.indexOf(mapString.trim().toUpperCase());
+			if(tmpIndex == -1) {
+				The5zigAPI.getAPI().messagePlayer(Log.error + "Map not found. Perhaps try adding it first?");
+				return true;
+			}
+			System.out.println(tmp + " / " + tmpIndex);
+			System.out.println(index);
+			maps.set(index - 1, mapString.trim().toUpperCase());
+			maps.set(tmpIndex, tmp);
+			
+			AutovoteUtils.set(gamemode.toLowerCase(), maps);
+			AutovoteUtils.dump();
+			The5zigAPI.getAPI().messagePlayer(Log.info + "Succesfully moved map.");
+			
 		} else {
-			The5zigAPI.getAPI().messagePlayer(
-					Log.info + "Usage:" + "\n" + "§b/autovote add mode_map §7- §3Adds a favorite map to a gamemode" + "\n"
+			The5zigAPI.getAPI()
+					.messagePlayer(Log.info + "Usage:" + "\n"
+							+ "§b/autovote add mode_map §7- §3Adds a favorite map to a gamemode" + "\n"
 							+ "§b/autovote list mode §7- §3Lists your favorite maps for that gamemode" + "\n"
-							+ "§b/autovote remove mode_map §7- §3Removes a favorite map from a gamemode");
+							+ "§b/autovote remove mode_map §7- §3Removes a favorite map from a gamemode\n" +
+							"§b/autovote place mode_map place§7- §3Puts the given map in [place] place.");
 		}
 
 		return true;
