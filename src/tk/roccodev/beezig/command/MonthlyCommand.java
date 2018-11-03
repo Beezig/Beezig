6df6467c106710018936ba1381a6c57c808f8eaf
RoccoDev <roccodev.business@gmail.com>
2018-11-03 16:43:35 +0100

    New wrapper in the monthly command

--- src/tk/roccodev/beezig/command/MonthlyCommand.java
+++ src/tk/roccodev/beezig/command/MonthlyCommand.java
@@ -1,6 +1,12 @@
 package tk.roccodev.beezig.command;
 
 import eu.the5zig.mod.The5zigAPI;
+import pw.roccodev.beezig.hiveapi.wrapper.monthly.bed.BedMonthlyProfile;
+import pw.roccodev.beezig.hiveapi.wrapper.monthly.maxthat.dr.DrMonthlyProfile;
+import pw.roccodev.beezig.hiveapi.wrapper.monthly.maxthat.timv.TimvMonthlyProfile;
+import pw.roccodev.beezig.hiveapi.wrapper.player.games.BedStats;
+import pw.roccodev.beezig.hiveapi.wrapper.player.games.DrStats;
+import pw.roccodev.beezig.hiveapi.wrapper.player.games.TimvStats;
 import tk.roccodev.beezig.ActiveGame;
 import tk.roccodev.beezig.Log;
 import tk.roccodev.beezig.hiveapi.HiveAPI;
@@ -28,6 +34,13 @@ public class MonthlyCommand implements Command {
     public boolean execute(String[] args) {
 
         if (args.length == 1 || args.length == 2) {
+            DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
+            df.setMinimumFractionDigits(2);
+            df.setMaximumFractionDigits(2);
+
+            DecimalFormat df1f = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
+            df1f.setMinimumFractionDigits(1);
+            df1f.setMaximumFractionDigits(1);
             try {
 
                 new Thread(() -> {
@@ -36,21 +49,13 @@ public class MonthlyCommand implements Command {
                     if (mode.equals("bed")) {
                         if (args[0].matches("-?\\d+")) {
                             int place = Integer.parseInt(args[0]);
-                            MonthlyPlayer monthly = ApiBED.getMonthlyStatusByPlace(place);
+                            BedMonthlyProfile monthly = new BedStats(null).getMonthlyProfile(place);
                             if (monthly == null) {
                                 The5zigAPI.getAPI().messagePlayer(Log.error + "No monthly profile found.");
                                 return;
                             }
                             double kd = (double) monthly.getKills() / (double) monthly.getDeaths();
-                            double wg = (double) monthly.getVictories() / (double) monthly.getPlayed() * 100D;
-
-                            DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
-                            df.setMinimumFractionDigits(2);
-                            df.setMaximumFractionDigits(2);
-
-                            DecimalFormat df1f = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
-                            df1f.setMinimumFractionDigits(1);
-                            df1f.setMaximumFractionDigits(1);
+                            double wg = (double) monthly.getVictories() / (double) monthly.getGamesPlayed() * 100D;
 
                             The5zigAPI.getAPI()
                                     .messagePlayer(Log.info + "§b#" + monthly.getPlace()
@@ -58,31 +63,24 @@ public class MonthlyCommand implements Command {
                                             + " §3points. (K/D: §b" + df.format(kd) + " §3| W/G: §b" + df1f.format(wg) + "%§3)");
 
                         } else {
-                            ApiBED api = new ApiBED(args[0]);
-                            MonthlyPlayer monthly = api.getMonthlyStatus();
+                            BedStats api = new BedStats(args[0], true);
+                            BedMonthlyProfile monthly = api.getMonthlyProfile();
                             if (monthly == null) {
                                 The5zigAPI.getAPI().messagePlayer(Log.error + "No monthly profile found.");
                                 return;
                             }
                             double kd = (double) monthly.getKills() / (double) monthly.getDeaths();
-                            double wg = (double) monthly.getVictories() / (double) monthly.getPlayed() * 100D;
+                            double wg = (double) monthly.getVictories() / (double) monthly.getGamesPlayed() * 100D;
 
-                            DecimalFormat df = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
-                            df.setMinimumFractionDigits(2);
-                            df.setMaximumFractionDigits(2);
-
-                            DecimalFormat df1f = (DecimalFormat) NumberFormat.getNumberInstance(Locale.US);
-                            df1f.setMinimumFractionDigits(1);
-                            df1f.setMaximumFractionDigits(1);
 
                             The5zigAPI.getAPI()
-                                    .messagePlayer(Log.info + "§b" + api.getParentMode().getCorrectName()
+                                    .messagePlayer(Log.info + "§b" + monthly.getUsername()
                                             + " §3is §b#" + monthly.getPlace() + " §3with §b" + monthly.getPoints()
                                             + " §3points. (K/D: §b" + df.format(kd) + " §3| W/G: §b" + df1f.format(wg) + "%§3)");
                         }
                         return;
                     }
-                    int index = Integer.parseInt(args[0]) - 1;
+                    int index = Integer.parseInt(args[0]);
                     if (mode.equals("timv")) {
                         mode = "TIMV";
                         unit = "karma.";
@@ -90,20 +88,26 @@ public class MonthlyCommand implements Command {
                         mode = "DR";
                         unit = "points.";
                     }
-                    String[] data = HiveAPI.getMonthlyLeaderboardsPlayerInfo(index, mode.toUpperCase()).split(",");
+
                     switch (mode) {
 
                         case "DR":
+                            DrMonthlyProfile profile = new DrStats(null).getMonthlyProfile(index);
+                            double wg = (double) profile.getVictories() / (double) profile.getGamesPlayed() * 100D;
+                            double ppg = (double) profile.getPoints() / (double) profile.getGamesPlayed();
                             The5zigAPI.getAPI()
-                                    .messagePlayer(Log.info + "§B#" + (index + 1) + "§3 is §B" + data[0] + "§3 with §B"
-                                            + data[1] + " §3" + unit + " (PpG: §B" + data[2] + " §3| W/L: §B" + data[3]
+                                    .messagePlayer(Log.info + "§B#" + profile.getHumanPlace() + "§3 is §B" + profile.getUsername() + "§3 with §B"
+                                            + profile.getPoints() + " §3" + unit + " (PpG: §B" + df.format(ppg) + " §3| W/L: §B" + df1f.format(wg)
                                             + "%§3)");
                             break;
 
                         case "TIMV":
+                            TimvMonthlyProfile timvProfile = new TimvStats(null).getMonthlyProfile(index);
+                            double kr = (double) timvProfile.getPoints() / (double) timvProfile.getRolePoints();
+                            double tr = (double) timvProfile.getTraitorPoints() / (double) timvProfile.getRolePoints() * 100D;
                             The5zigAPI.getAPI()
-                                    .messagePlayer(Log.info + "§B#" + (index + 1) + "§3 is §B" + data[0] + "§3 with §B"
-                                            + data[1] + " §3" + unit + " (K/R: §B" + data[2] + " §3| T/R: " + data[3]
+                                    .messagePlayer(Log.info + "§B#" + timvProfile.getHumanPlace() + "§3 is §B" + timvProfile.getUsername() + "§3 with §B"
+                                            + timvProfile.getPoints() + " §3" + unit + " (K/R: §B" + df.format(kr) + " §3| T/R: " + df1f.format(tr)
                                             + "%§3)");
                             break;
 
