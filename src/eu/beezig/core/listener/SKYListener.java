77d5a0dc722068dc37a3d30502038abb2a346eec
RoccoDev <roccodev.business@gmail.com>
2019-04-13 13:46:58 +0200

    Fix headers

--- src/eu/beezig/core/listener/SKYListener.java
+++ src/eu/beezig/core/listener/SKYListener.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2019 Beezig (RoccoDev, ItsNiklass)
+ *
+ * This file is part of Beezig.
+ *
+ * Beezig is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Beezig is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Beezig.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package eu.beezig.core.listener;
 
 import eu.beezig.core.ActiveGame;
@@ -24,7 +43,6 @@ import pw.roccodev.beezig.hiveapi.wrapper.player.HivePlayer;
 import pw.roccodev.beezig.hiveapi.wrapper.player.games.SkyStats;
 
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.util.*;
